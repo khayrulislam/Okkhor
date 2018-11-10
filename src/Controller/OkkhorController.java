18c1bb3bcf0ac8c9870731e0bfe9b0d86164cb74
olife <olife@192.168.1.109>
2018-11-10 13:16:08 +0600

    suggestion list added

--- src/Controller/OkkhorController.java
+++ src/Controller/OkkhorController.java
@@ -1,17 +1,25 @@
 package Controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 import CurrentStatus.UserStatus;
 import Utils.Util;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class OkkhorController extends WindowTransition implements Initializable {
 	
@@ -20,6 +28,9 @@ public class OkkhorController extends WindowTransition implements Initializable 
 	
 	@FXML
 	TextField okkhorTf;
+	
+	@FXML
+	VBox okkhorSuggestionVb;
 	
 	
 	@FXML
@@ -39,6 +50,61 @@ public class OkkhorController extends WindowTransition implements Initializable 
 		
 		if(userStatus.getStatus().equals(Util.DEFAULT)) logOutBt.setText("পিছনে");
 		
+	}
+	
+	
+	private void createSuggestionList() {
+		
+		ArrayList<String> list = new ArrayList<>(Arrays.asList("olife ddddddddddddddddddddddddd","atiq","jamil","thamid","jubair"));
+		
+		for(int i=0;i<list.size();i++) {
+			
+			okkhorSuggestionVb.getChildren().add(getASuggestionItem(list.get(i)));
+			
+		}
+		
+	}
+	
+	private Label getASuggestionItem(String text) {
+		
+		Label label = new Label(text);
+		label.setId("okkhorSuggestionLabel");
+		
+		label.setOnMouseEntered(new EventHandler<Event>() {
+
+			@Override
+			public void handle(Event event) {
+				// TODO Auto-generated method stub
+				label.setId("okkhorSuggestionLabelFocused");
+			}
+		});
+		
+		label.setOnMouseExited(new EventHandler<Event>() {
+
+			@Override
+			public void handle(Event event) {
+				// TODO Auto-generated method stub
+				label.setId("okkhorSuggestionLabel");
+			}
+		});
+		
+		label.setOnMouseClicked(new EventHandler<Event>() {
+
+			@Override
+			public void handle(Event event) {
+				// TODO Auto-generated method stub
+				System.out.println(label.getText());
+				okkhorTf.setText(label.getText());
+				okkhorSuggestionVb.getChildren().clear();
+			}
+		});
+		
+		return label;
+	}
+	
+	
+	public void ff(ActionEvent event) {
+		createSuggestionList();
 	}
 	
 	
